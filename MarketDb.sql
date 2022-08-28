00d5e925a7f1c85126bc61a39efe9b0f96d99820
ali-moussawi <aha057@usal.edu.lb>
2022-08-28 12:16:20 +0300

    implement Database v:1

---
+++ MarketDb.sql
@@ -0,0 +1,307 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: marketpricing
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `categoryId` int NOT NULL AUTO_INCREMENT,
+  `categoryName` varchar(45) NOT NULL,
+  PRIMARY KEY (`categoryId`),
+  UNIQUE KEY `categoryName_UNIQUE` (`categoryName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `loginlogs`
+--
+
+DROP TABLE IF EXISTS `loginlogs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `loginlogs` (
+  `LogId` int NOT NULL AUTO_INCREMENT,
+  `userGmail` varchar(55) NOT NULL,
+  `LoginDate` date NOT NULL,
+  PRIMARY KEY (`LogId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `loginlogs`
+--
+
+LOCK TABLES `loginlogs` WRITE;
+/*!40000 ALTER TABLE `loginlogs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `loginlogs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permissions`
+--
+
+DROP TABLE IF EXISTS `permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `permissions` (
+  `PermissionId` int NOT NULL AUTO_INCREMENT,
+  `PermissionName` varchar(45) NOT NULL,
+  PRIMARY KEY (`PermissionId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permissions`
+--
+
+LOCK TABLES `permissions` WRITE;
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productdetials`
+--
+
+DROP TABLE IF EXISTS `productdetials`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productdetials` (
+  `ProductId` int NOT NULL,
+  `price` double NOT NULL,
+  `Date` date NOT NULL,
+  PRIMARY KEY (`ProductId`,`Date`),
+  CONSTRAINT `pid` FOREIGN KEY (`ProductId`) REFERENCES `supermarketproducts` (`ProductId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productdetials`
+--
+
+LOCK TABLES `productdetials` WRITE;
+/*!40000 ALTER TABLE `productdetials` DISABLE KEYS */;
+/*!40000 ALTER TABLE `productdetials` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `ProductId` int NOT NULL AUTO_INCREMENT,
+  `ProductName` varchar(55) NOT NULL,
+  `BarcodeNb` int NOT NULL,
+  PRIMARY KEY (`ProductId`),
+  UNIQUE KEY `ProductName_UNIQUE` (`ProductName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rolepermissions`
+--
+
+DROP TABLE IF EXISTS `rolepermissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rolepermissions` (
+  `RoleId` int NOT NULL,
+  `PermissionsId` int NOT NULL,
+  PRIMARY KEY (`RoleId`,`PermissionsId`),
+  KEY `forg2_idx` (`PermissionsId`),
+  CONSTRAINT `forg1` FOREIGN KEY (`RoleId`) REFERENCES `roles` (`RoleId`),
+  CONSTRAINT `forg2` FOREIGN KEY (`PermissionsId`) REFERENCES `permissions` (`PermissionId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rolepermissions`
+--
+
+LOCK TABLES `rolepermissions` WRITE;
+/*!40000 ALTER TABLE `rolepermissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rolepermissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `RoleId` int NOT NULL AUTO_INCREMENT,
+  `RoleName` varchar(50) NOT NULL,
+  PRIMARY KEY (`RoleId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `supermarket`
+--
+
+DROP TABLE IF EXISTS `supermarket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `supermarket` (
+  `supermarketId` int NOT NULL AUTO_INCREMENT,
+  `supermarketName` varchar(45) NOT NULL,
+  `supermarketRegion` varchar(45) NOT NULL,
+  `phoneNumber` varchar(45) NOT NULL,
+  PRIMARY KEY (`supermarketId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `supermarket`
+--
+
+LOCK TABLES `supermarket` WRITE;
+/*!40000 ALTER TABLE `supermarket` DISABLE KEYS */;
+/*!40000 ALTER TABLE `supermarket` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `supermarketproducts`
+--
+
+DROP TABLE IF EXISTS `supermarketproducts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `supermarketproducts` (
+  `SuperMarketId` int NOT NULL,
+  `ProductId` int NOT NULL,
+  `CategoryId` int NOT NULL,
+  PRIMARY KEY (`SuperMarketId`,`ProductId`),
+  KEY `fo2_idx` (`ProductId`),
+  KEY `fo3_idx` (`CategoryId`),
+  CONSTRAINT `fo1` FOREIGN KEY (`SuperMarketId`) REFERENCES `supermarket` (`supermarketId`) ON DELETE CASCADE,
+  CONSTRAINT `fo2` FOREIGN KEY (`ProductId`) REFERENCES `products` (`ProductId`) ON DELETE CASCADE,
+  CONSTRAINT `fo3` FOREIGN KEY (`CategoryId`) REFERENCES `categories` (`categoryId`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `supermarketproducts`
+--
+
+LOCK TABLES `supermarketproducts` WRITE;
+/*!40000 ALTER TABLE `supermarketproducts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `supermarketproducts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `temppermissions`
+--
+
+DROP TABLE IF EXISTS `temppermissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `temppermissions` (
+  `UserId` int NOT NULL,
+  `PermissionId` int NOT NULL,
+  PRIMARY KEY (`UserId`,`PermissionId`),
+  KEY `f2_idx` (`PermissionId`),
+  CONSTRAINT `f1` FOREIGN KEY (`UserId`) REFERENCES `users` (`UserId`),
+  CONSTRAINT `f2` FOREIGN KEY (`PermissionId`) REFERENCES `permissions` (`PermissionId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `temppermissions`
+--
+
+LOCK TABLES `temppermissions` WRITE;
+/*!40000 ALTER TABLE `temppermissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `temppermissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `UserId` int NOT NULL AUTO_INCREMENT,
+  `RoleId` int NOT NULL,
+  `Gmail` varchar(55) NOT NULL,
+  `Password` varchar(55) NOT NULL,
+  `Name` varchar(55) NOT NULL,
+  `PhoneNumber` varchar(10) NOT NULL,
+  PRIMARY KEY (`UserId`),
+  UNIQUE KEY `Gmail_UNIQUE` (`Gmail`),
+  KEY `RoleId_idx` (`RoleId`),
+  CONSTRAINT `RoleId` FOREIGN KEY (`RoleId`) REFERENCES `roles` (`RoleId`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-08-28 12:13:36
