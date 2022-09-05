58f47cda6a9cec1e85b8816ddfbb95767300f42d
ali-moussawi <aha057@usal.edu.lb>
2022-09-05 21:26:59 +0300

    working on productList for each supermarket and downloading sql connector to make connection with mysql and use queries directly

--- MarketPricingSystem/MarketPricingSystem/Controllers/HomeController.cs
+++ MarketPricingSystem/MarketPricingSystem/Controllers/HomeController.cs
@@ -5,12 +5,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using MarketPricingSystem.ViewModel;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace MarketPricingSystem.Controllers
 {
+    
     public class HomeController : Controller
     {
-       private marketpricingContext _context;
+      
+        private marketpricingContext _context;
 
         public HomeController()
         {
@@ -58,9 +64,44 @@ namespace MarketPricingSystem.Controllers
 
         public ActionResult ProductList(int id)
         {
-           // var allproducts = _context.Supermarketproducts.Where(s => s.SuperMarketId == id).Include(x => x.Productprices).ToList();
-            return View();
+
+            List<productdetails> products = new List<productdetails>();
+       
+       //install sql connector to can open connection
+            using (MySqlConnection con = new MySqlConnection("server=localhost;user=root;password=123;database=marketpricing"))
+            {
+                string query = "Select productname, BarcodeNb, productDescription, price from products p , productprices pc where pc.supermarketid = "+id.ToString()+ "  and p.productid = pc.productid and pc.IsActivePrice=0";
+                using (MySqlCommand cmd = new MySqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (MySqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            products.Add(new productdetails
+                            {
+                                productname = sdr["productname"].ToString(),
+                                productBarcode = Convert.ToInt32(sdr["BarcodeNb"]),
+                                productDescription = sdr["productDescription"].ToString(),
+                                productprice = Convert.ToInt32(sdr["price"]),
+
+                            });
+                        }
+                    }
+                    con.Close();
+                }
+            }
+
+            return View(products);
+
+            
 
         }
+
+      
+
     }
+
+
 }
---
+++ MarketPricingSystem/MarketPricingSystem/ViewModel/productdetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarketPricingSystem.ViewModel
+{
+    public class productdetails
+    {
+
+        public string productname { get; set; }
+
+        public int productBarcode { get; set; }
+        public string productDescription { get; set; }
+
+        public int productprice { get; set; }
+
+       // public DateTime date { get; set; }
+
+
+
+    }
+}
