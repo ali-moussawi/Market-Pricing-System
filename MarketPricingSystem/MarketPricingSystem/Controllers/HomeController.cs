cc0370b7fbf840d7a06f394f4f693c6ed517a390
ali-moussawi <aha057@usal.edu.lb>
2022-09-05 23:34:03 +0300

    working on cheapest and expensive products overall supermarkets

--- MarketPricingSystem/MarketPricingSystem/Controllers/HomeController.cs
+++ MarketPricingSystem/MarketPricingSystem/Controllers/HomeController.cs
@@ -85,7 +85,7 @@ namespace MarketPricingSystem.Controllers
                                 productBarcode = Convert.ToInt32(sdr["BarcodeNb"]),
                                 productDescription = sdr["productDescription"].ToString(),
                                 productprice = Convert.ToInt32(sdr["price"]),
-
+                                //category for each product
                             });
                         }
                     }
@@ -99,7 +99,106 @@ namespace MarketPricingSystem.Controllers
 
         }
 
-      
+        public ActionResult Allproducts()
+        {
+            List<AllProducts> products = new List<AllProducts>();
+
+            //install sql connector to can open connection
+            using (MySqlConnection con = new MySqlConnection("server=localhost;user=root;password=123;database=marketpricing"))
+            {
+                string query = "select productid, productname, barcodenb, productdescription from products p where p.productid in (select distinct productid from productprices);";
+                using (MySqlCommand cmd = new MySqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (MySqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            products.Add(new AllProducts
+                            {
+                                productid = Convert.ToInt32(sdr["productid"]),
+                                productname = sdr["productname"].ToString(),
+                                productBarcode = Convert.ToInt32(sdr["barcodenb"]),
+                                productDescription= sdr["productdescription"].ToString(),
+                            });
+                        }
+                    }
+                    con.Close();
+                }
+            }
+
+            return View(products);
+
+        }
+
+        public ActionResult GetCheapestproduct(int id)
+        {
+
+            productvalue cheapproduct=new productvalue();
+            using (MySqlConnection con = new MySqlConnection("server=localhost;user=root;password=123;database=marketpricing"))
+            {
+                string query = " select supermarketname , productname , price  from supermarket s, products p , productprices pc where s.supermarketid = pc.supermarketid and p.productid = pc.productid and p.productid ="+id.ToString()+ " and pc.ISactiveprice=0    ORDER BY price ASC   limit 1 ;";
+                using (MySqlCommand cmd = new MySqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (MySqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                             cheapproduct = new productvalue
+                            {
+                                supermarketname = sdr["supermarketname"].ToString(),
+                                productname = sdr["productname"].ToString(),
+                                productprice = Convert.ToInt32(sdr["price"]),
+
+                            };
+                          
+                        }
+                    }
+                    con.Close();
+                }
+
+            }
+            return View(cheapproduct);
+
+        }
+
+
+        public ActionResult GetExpensiveproduct(int id)
+        {
+
+            productvalue cheapproduct = new productvalue();
+            using (MySqlConnection con = new MySqlConnection("server=localhost;user=root;password=123;database=marketpricing"))
+            {
+                string query = " select supermarketname , productname , price  from supermarket s, products p , productprices pc where s.supermarketid = pc.supermarketid and p.productid = pc.productid and p.productid =" + id.ToString() + " and pc.ISactiveprice=0    ORDER BY price DESC   limit 1 ;";
+                using (MySqlCommand cmd = new MySqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (MySqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            cheapproduct = new productvalue
+                            {
+                                supermarketname = sdr["supermarketname"].ToString(),
+                                productname = sdr["productname"].ToString(),
+                                productprice = Convert.ToInt32(sdr["price"]),
+
+                            };
+
+                        }
+                    }
+                    con.Close();
+                }
+
+            }
+            return View(cheapproduct);
+
+        }
+
 
     }
 
--- MarketPricingSystem/MarketPricingSystem/ViewModel/AllProducts.cs
+++ MarketPricingSystem/MarketPricingSystem/ViewModel/AllProducts.cs
@@ -5,19 +5,14 @@ using System.Web;
 
 namespace MarketPricingSystem.ViewModel
 {
-    public class productdetails
+    public class AllProducts
     {
-
+        public int productid { get; set; }
         public string productname { get; set; }
 
         public int productBarcode { get; set; }
         public string productDescription { get; set; }
 
-        public int productprice { get; set; }
-
-      //  public string category { get; set; }
-
-
-
+        //CATEGORY
     }
 }
