30279f589578ad46fa52aa5038dc89d2993063fc
ali-moussawi <aha057@usal.edu.lb>
2022-09-09 20:23:35 +0300

    working on dashboard

---
+++ MarketPricingSystem/MarketPricingSystem/ViewModel/supermarketdata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarketPricingSystem.ViewModel
+{
+    public class supermarketdata
+    {
+        public string SupermarketName { get; set; }
+        public string SupermarketRegion { get; set; }
+        public string SupermarketDescription { get; set; }
+
+
+    }
+}
